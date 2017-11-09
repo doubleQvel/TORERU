edb32b485d7f4c7fccce65cde09c8d59b2fb4355
Matsumoto <rxn302n1@yahoo.co.jp>
2017-11-09 11:21:28 +0900

    Initialize Application

---
+++ TORERU/TORERUTests/TORERUTests.swift
@@ -0,0 +1,36 @@
+//
+//  TORERUTests.swift
+//  TORERUTests
+//
+//  Created by Matsumoto Kazuki on 2017/11/09.
+//  Copyright © 2017年 Matsumoto Kazuki. All rights reserved.
+//
+
+import XCTest
+@testable import TORERU
+
+class TORERUTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
