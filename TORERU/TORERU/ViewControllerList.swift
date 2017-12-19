406efa73ca03a6cc43d001cd5b3b7e2d94d317f0
kinakodai <e16574@kina.local>
2017-12-19 16:50:03 +0900

    リストの作成

--- TORERU/TORERU/ViewControllerList.swift
+++ TORERU/TORERU/ViewControllerList.swift
@@ -8,14 +8,49 @@
 
 import UIKit
 
-class ViewControllerList: UIViewController {
+class ViewControllerList: UIViewController, UICollectionViewDataSource,
+UICollectionViewDelegate{
 
+    @IBOutlet weak var listview: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        listview.delegate = self
+        listview.dataSource = self
         // Do any additional setup after loading the view.
     }
-
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        // "Cell" はストーリーボードで設定したセルのID
+        let testCell:UICollectionViewCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
+                                               for: indexPath)
+        
+        // Tag番号を使ってImageViewのインスタンス生成
+        
+        // 画像配列の番号で指定された要素の名前の画像をUIImageとする
+        
+        // UIImageをUIImageViewのimageとして設定
+     
+        
+        // Tag番号を使ってLabelのインスタンス生成
+        let label = testCell.contentView.viewWithTag(1) as! UILabel
+        let name = ["時限","月","火","水","木","金","1","","","","","","2","","","","","","3","","os","","","","4","","","","","","5"]
+        label.text = name[indexPath.row]
+        
+        return testCell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // section数は１つ
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        // 要素数を入れる、要素以上の数字を入れると表示でエラーとなる
+        return 31;
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
